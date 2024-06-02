b51e83dd8165ca1bdb9cd89d5c03250ce2aa5404
HLS <hannahsilverb@gmail.com>
2024-06-02 10:35:37 +0300

    chore: created component folder, and moved things there

--- front_jsx/src/App.js
+++ front_jsx/src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import question from './HelpQuestionnaire';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          {/* <li><a href="/tools/help-score">HELP Score</a></li> */}
-          <Routes>
-          <Route path="/questionnaire" element={<HelpQuestionnaire />} />
-          </Routes>
-        </header>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
