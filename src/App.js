67fbf1be0a664edc85a713ad0934dbb106aeda1d
Gil <gil.yair.yamin@gmail.com>
2024-06-11 12:59:30 +0300

    init TextEditor

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import HomePage from './screens/Homepage/Homepage';
@@ -9,11 +10,24 @@ import AdminDashboard from './screens/AdminDashboard/AdminDashboard';
 import HelpScoreForm from './components/helpScore/HelpScoreForm';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 import './App.css';
+=======
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
+import HomePage from './screens/Homepage/Homepage'
+import Signup from './screens/Signup/Signup'
+import Login from './screens/Login/Login'
+import Navbar from './components/NavBar/NavBar'
+import Contact from './screens/Contact/Contact'
+import AdminDashboard from './screens/AdminDashboard/AdminDashboard'
+import EditPostPage from './screens/EditPost/TextEditor'
+import './App.css'
+>>>>>>> Stashed changes
 
 function App() {
-  const location = useLocation();
-  const isAdminRoute = location.pathname.startsWith('/admin');
+	const location = useLocation()
+	const isAdminRoute = location.pathname.startsWith('/admin')
 
+<<<<<<< Updated upstream
   return (
     <div className="App">
       {!isAdminRoute && <Navbar />}
@@ -31,12 +45,27 @@ function App() {
       </Routes>
     </div>
   );
+=======
+	return (
+		<div className='App'>
+			{!isAdminRoute && <Navbar />}
+			<Routes>
+				<Route exact path='/' element={<HomePage />} />
+				<Route exact path='/signup' element={<Signup />} />
+				<Route exact path='/login' element={<Login />} />
+				<Route exact path='/contact' element={<Contact />} />
+				<Route path='/admin/*' element={<AdminDashboard />} />
+				<Route exact path='/edit' element={<EditPostPage />} />
+			</Routes>
+		</div>
+	)
+>>>>>>> Stashed changes
 }
 
 const AppWrapper = () => (
-  <Router>
-    <App />
-  </Router>
-);
+	<Router>
+		<App />
+	</Router>
+)
 
-export default AppWrapper;
+export default AppWrapper
