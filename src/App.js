48b309b7d8af21882a65e6243855d1059d777ccd
Gil <gil.yair.yamin@gmail.com>
2024-06-13 19:30:51 +0300

    added a lot of stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import HomePage from './screens/Homepage/Homepage';
-import Signup from './screens/Signup/Signup';
-import Login from './screens/Login/Login';
-import Navbar from './components/NavBar/NavBar';
-import Contact from './screens/Contact/Contact';
-import AdminDashboard from './screens/AdminDashboard/AdminDashboard';
-import HelpScoreForm from './components/helpScore/HelpScoreForm';
-import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import './App.css';
-=======
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom'
 import HomePage from './screens/Homepage/Homepage'
@@ -21,31 +8,11 @@ import Contact from './screens/Contact/Contact'
 import AdminDashboard from './screens/AdminDashboard/AdminDashboard'
 import EditPostPage from './screens/EditPost/TextEditor'
 import './App.css'
->>>>>>> Stashed changes
 
 function App() {
 	const location = useLocation()
 	const isAdminRoute = location.pathname.startsWith('/admin')
 
-<<<<<<< Updated upstream
-  return (
-    <div className="App">
-      {!isAdminRoute && <Navbar />}
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/signup" element={<Signup />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/helpScore" element={<HelpScoreForm />} />
-        <Route path="/admin/*" element={
-          <ProtectedRoute>
-            <AdminDashboard />
-          </ProtectedRoute>
-        } />
-      </Routes>
-    </div>
-  );
-=======
 	return (
 		<div className='App'>
 			{!isAdminRoute && <Navbar />}
@@ -59,7 +26,6 @@ function App() {
 			</Routes>
 		</div>
 	)
->>>>>>> Stashed changes
 }
 
 const AppWrapper = () => (
--- src/screens/EditPost/TextEditor.jsx
+++ src/screens/EditPost/TextEditor.jsx
@@ -1,13 +1,47 @@
 import './TextEditor.css'
 
+import React from 'react'
+
 import { Color } from '@tiptap/extension-color'
 import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
 import { EditorProvider, useCurrentEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import React from 'react'
+import Underline from '@tiptap/extension-underline'
+import TextDirection from 'tiptap-text-direction'
+import { bulletList, orderedList } from '@tiptap/pm/schema-list'
 
-import { FaBold, FaItalic, FaStrikethrough } from 'react-icons/fa'
+import {
+	FaBold,
+	FaItalic,
+	FaStrikethrough,
+	FaUnderline,
+	FaUndo,
+	FaRedo,
+	FaListUl,
+	FaListOl,
+	FaAlignLeft,
+	FaAlignCenter,
+	FaAlignRight,
+	FaAlignJustify,
+} from 'react-icons/fa'
+import TextAlign from '@tiptap/extension-text-align'
+
+import { MdOutlineFormatTextdirectionRToL, MdFormatTextdirectionLToR } from 'react-icons/md'
+
+const getFontSize = (elementType) => {
+	// Create a temporary h1 element
+	const tempH1 = document.createElement(elementType, { visibility: 'hidden' })
+	tempH1.style.visibility = 'hidden' // Make it invisible
+	document.body.appendChild(tempH1)
+
+	// Get the computed font size
+	const fontSize = window.getComputedStyle(tempH1).fontSize
+
+	// Remove the temporary h1 element
+	document.body.removeChild(tempH1)
+	return fontSize
+}
 
 const MenuBar = () => {
 	const { editor } = useCurrentEditor()
@@ -16,116 +50,147 @@ const MenuBar = () => {
 		return null
 	}
 
+	const onFontSizeChange = (e) => {
+		const fontSize = e.target.value
+		for (let i = 1; i <= 6; i++) {
+			if (editor.isActive('heading', { level: 1 }))
+				editor.chain().focus().toggleHeading({ level: 1 }).run()
+		}
+		editor.chain().focus().setParagraph().run()
+
+		switch (fontSize) {
+			case 'h5':
+				editor.chain().focus().toggleHeading({ level: 5 }).run()
+				break
+			case 'h4':
+				editor.chain().focus().toggleHeading({ level: 4 }).run()
+				break
+			case 'h3':
+				editor.chain().focus().toggleHeading({ level: 3 }).run()
+				break
+			case 'h2':
+				editor.chain().focus().toggleHeading({ level: 2 }).run()
+				break
+			case 'h1':
+				editor.chain().focus().toggleHeading({ level: 1 }).run()
+				break
+		}
+	}
+
 	return (
-		<div className='menubar'>
-			<button
-				onClick={() => editor.chain().focus().toggleBold().run()}
-				disabled={!editor.can().chain().focus().toggleBold().run()}
-				className={`editor-button ${editor.isActive('bold') ? 'editor-button-is-active' : ''}`}>
-				<FaBold />
-			</button>
+		<div className='tiptap menu-bar'>
+			<div>
+				<button
+					onClick={() => editor.chain().focus().undo().run()}
+					disabled={!editor.can().chain().focus().undo().run()}
+					className={`use-button`}>
+					<FaRedo />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().redo().run()}
+					disabled={!editor.can().chain().focus().redo().run()}
+					className={`use-button`}>
+					<FaUndo />
+				</button>
+			</div>
+			<div>
+				<button
+					onClick={() => editor.chain().focus().toggleBold().run()}
+					disabled={!editor.can().chain().focus().toggleBold().run()}
+					className={`${editor.isActive('bold') ? 'is-active' : ''}`}>
+					<FaBold />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleItalic().run()}
+					disabled={!editor.can().chain().focus().toggleItalic().run()}
+					className={`${editor.isActive('italic') ? 'is-active' : ''}`}>
+					<FaItalic />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleUnderline().run()}
+					disabled={!editor.can().chain().focus().toggleUnderline().run()}
+					className={` ${editor.isActive('underline') ? 'is-active' : ''}`}>
+					<FaUnderline />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleStrike().run()}
+					disabled={!editor.can().chain().focus().toggleStrike().run()}
+					className={`${editor.isActive('strike') ? 'is-active' : ''}`}>
+					<FaStrikethrough />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleBulletList().run()}
+					className={`${editor.isActive('bulletList') ? 'is-active' : ''} `}>
+					<FaListUl />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().toggleOrderedList().run()}
+					className={`${editor.isActive('orderedList') ? 'is-active' : ''}`}>
+					<FaListOl />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextAlign('right').run()}
+					className={editor.isActive({ textAlign: 'right' }) ? 'is-active' : ''}>
+					<FaAlignRight />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextAlign('center').run()}
+					className={editor.isActive({ textAlign: 'center' }) ? 'is-active' : ''}>
+					<FaAlignCenter />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextAlign('left').run()}
+					className={editor.isActive({ textAlign: 'left' }) ? 'is-active' : ''}>
+					<FaAlignLeft />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextAlign('justify').run()}
+					className={editor.isActive({ textAlign: 'justify' }) ? 'is-active' : ''}>
+					<FaAlignJustify />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextDirection('rtl').run()}
+					className={editor.isActive({ dir: 'rtl' }) ? 'is-active' : ''}>
+					<MdOutlineFormatTextdirectionRToL />
+				</button>
+				<button
+					onClick={() => editor.chain().focus().setTextDirection('ltr').run()}
+					className={editor.isActive({ dir: 'ltr' }) ? 'is-active' : ''}>
+					<MdFormatTextdirectionLToR />
+				</button>
 
-			<button
-				onClick={() => editor.chain().focus().toggleItalic().run()}
-				disabled={!editor.can().chain().focus().toggleItalic().run()}
-				className={`editor-button ${editor.isActive('italic') ? 'editor-button-is-active' : ''}`}>
-				<FaItalic />
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleStrike().run()}
-				disabled={!editor.can().chain().focus().toggleStrike().run()}
-				className={`editor-button ${editor.isActive('strike') ? 'editor-button-is-active' : ''}`}>
-				<FaStrikethrough />
-			</button>
-
-			<button
-				onClick={() => console.log(editor.getHTML())}
-				disabled={!editor.can().chain().focus().toggleStrike().run()}
-				className={`editor-button ${editor.isActive('strike') ? 'editor-button-is-active' : ''}`}>
-				test
-			</button>
-
-			<button onClick={() => editor.chain().focus().unsetAllMarks().run()}>clear marks</button>
-			<button onClick={() => editor.chain().focus().clearNodes().run()}>clear nodes</button>
-			<button
-				onClick={() => editor.chain().focus().setParagraph().run()}
-				className={editor.isActive('paragraph') ? 'is-active' : ''}>
-				paragraph
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-				className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}>
-				h1
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-				className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}>
-				h2
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-				className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}>
-				h3
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-				className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}>
-				h4
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-				className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}>
-				h5
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-				className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}>
-				h6
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleBulletList().run()}
-				className={editor.isActive('bulletList') ? 'is-active' : ''}>
-				bullet list
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleOrderedList().run()}
-				className={editor.isActive('orderedList') ? 'is-active' : ''}>
-				ordered list
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-				className={editor.isActive('codeBlock') ? 'is-active' : ''}>
-				code block
-			</button>
-			<button
-				onClick={() => editor.chain().focus().toggleBlockquote().run()}
-				className={editor.isActive('blockquote') ? 'is-active' : ''}>
-				blockquote
-			</button>
-			<button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-				horizontal rule
-			</button>
-			<button onClick={() => editor.chain().focus().setHardBreak().run()}>hard break</button>
-			<button
-				onClick={() => editor.chain().focus().undo().run()}
-				disabled={!editor.can().chain().focus().undo().run()}>
-				undo
-			</button>
-			<button
-				onClick={() => editor.chain().focus().redo().run()}
-				disabled={!editor.can().chain().focus().redo().run()}>
-				redo
-			</button>
-			<button
-				onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-				className={editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}>
-				purple
-			</button>
+				<select onChange={onFontSizeChange} className='editor-button'>
+					<option value='h5' style={{ fontSize: `${getFontSize('h5')}` }}>
+						קטן מאוד
+					</option>
+					<option value='p' style={{ fontSize: `${getFontSize('p')}` }}>
+						קטן
+					</option>
+					<option value='h3' style={{ fontSize: `${getFontSize('h3')}` }}>
+						בינוני
+					</option>
+					<option value='h2' style={{ fontSize: `${getFontSize('h2')}` }}>
+						גדול
+					</option>
+					<option value='h1' style={{ fontSize: `${getFontSize('h1')}` }}>
+						ענק
+					</option>
+				</select>
+			</div>
 		</div>
 	)
 }
 
 const extensions = [
+	TextDirection.configure({
+		types: ['heading', 'paragraph', StarterKit.bulletList.name],
+		defaultDirection: 'rtl',
+	}),
+	TextAlign.configure({
+		types: ['heading', 'paragraph', bulletList.name],
+		alignments: ['left', 'center', 'right', 'justify'],
+	}),
+	Underline,
 	Color.configure({ types: [TextStyle.name, ListItem.name] }),
 	TextStyle.configure({ types: [ListItem.name] }),
 	StarterKit.configure({
@@ -175,9 +240,11 @@ const TextEditor = () => {
 	return (
 		<>
 			<EditorProvider
+				className='tiptap'
 				slotBefore={<MenuBar />}
 				extensions={extensions}
-				content={content}></EditorProvider>
+				content={content}
+			/>
 		</>
 	)
 }
