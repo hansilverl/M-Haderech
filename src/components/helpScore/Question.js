6a9de72851277d2ceaeb8ec1ebc0a4f2186256f6
HLS <hannahsilverb@gmail.com>
2024-06-13 20:26:24 +0300

    refactor: Update HelpScoreForm component and style

--- src/components/helpScore/Question.js
+++ src/components/helpScore/Question.js
@@ -1,5 +1,41 @@
-// src/components/helpScore/Question.js
 import React, { useState } from 'react';
+
+const OptionHeaders = ({ options }) => (
+    <thead>
+        <tr>
+            {Object.keys(options).map((option) => (
+                <th key={option}>
+                    {options[option]}
+                </th>
+            ))}
+        </tr>
+    </thead>
+);
+
+const OptionCells = ({ options, selectedOption, handleRadioChange, name }) => (
+    <tbody>
+        <tr>
+            {Object.keys(options).map((option) => (
+                <td
+                    key={option}
+                    className={selectedOption === option ? 'selected' : ''}
+                >
+                    <label className="radio-label">
+                        <input
+                            type="radio"
+                            name={name}
+                            value={option}
+                            checked={selectedOption === option}
+                            onChange={() => handleRadioChange(option)}
+                            className="radio-input"
+                        />
+                        <span className="custom-radio"></span>
+                    </label>
+                </td>
+            ))}
+        </tr>
+    </tbody>
+);
 
 const Question = ({ question }) => {
     const { id, q, ...options } = question;
@@ -11,38 +47,15 @@ const Question = ({ question }) => {
 
     return (
         <div className="q-container">
-            <p id={`question-${id}`} className="question-text">{q}</p>            
+            <p id={`question-${id}`} className="question-text">{q}</p>
             <table className="q-table">
-                <thead>
-                    <tr>
-                        {Object.keys(options).map((option) => (
-                            <th key={option}>
-                                {options[option]}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        {Object.keys(options).map((option) => (
-                            <td
-                                key={option}
-                                className={selectedOption === option ? 'selected' : ''}
-                            >
-                                <label className="radio-label">
-                                    <input
-                                        type="radio"
-                                        name={id}
-                                        value={option}
-                                        checked={selectedOption === option}
-                                        onChange={() => handleRadioChange(option)}
-                                        className="radio-input"
-                                    />
-                                </label>
-                            </td>
-                        ))}
-                    </tr>
-                </tbody>
+                <OptionHeaders options={options} />
+                <OptionCells 
+                    options={options} 
+                    selectedOption={selectedOption} 
+                    handleRadioChange={handleRadioChange} 
+                    name={id} 
+                />
             </table>
         </div>
     );
