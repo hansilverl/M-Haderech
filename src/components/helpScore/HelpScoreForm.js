6b3a2f936941178b43656a1865ed5e25a02c0f05
HLS <hannahsilverb@gmail.com>
2024-06-13 21:02:54 +0300

    Storage pt 1

--- src/components/helpScore/HelpScoreForm.js
+++ src/components/helpScore/HelpScoreForm.js
@@ -1,35 +1,40 @@
-// src/components/helpScore/HelpScoreForm.js
-import React from 'react'
-import useHelpScore from '../../hooks/useHelpScore'
-import Question from './Question'
-import './HelpScoreForm.css'
-
-
+import React, { useState } from 'react';
+import useHelpScore from '../../hooks/useHelpScore';
+import Question from './Question';
+import './HelpScoreForm.css';
 
 const HelpScoreForm = () => {
-    const { questions, loading, error } = useHelpScore()
+    const { questions, loading, error } = useHelpScore();
+    const [answers, setAnswers] = useState({}); // Assuming answers are stored as an object
+
+    const handleRadioChange = (questionId, selectedValue) => {
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            [questionId]: selectedValue,
+        }));
+    };
 
     if (loading) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
 
     if (error) {
-        return <div>Error: {error.message}</div>
+        return <div>Error: {error.message}</div>;
     }
 
-const descriptionQuestion = questions[0]
-return (
-    <div className="help-score-form">
-        <div className="bordered-container">
-            <Question question={descriptionQuestion} />
-            <div>
-                {questions.slice(1).map((question) => (
-                    <Question key={question.id} question={question} />
-                ))}
+    const descriptionQuestion = questions[0];
+    return (
+        <div className="help-score-form">
+            <div className="bordered-container">
+                <Question question={descriptionQuestion} onRadioChange={handleRadioChange} />
+                <div>
+                    {questions.slice(1).map((question) => (
+                        <Question key={question.id} question={question} onRadioChange={handleRadioChange} />
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);
-}
+    );
+};
 
-export default HelpScoreForm
+export default HelpScoreForm;
