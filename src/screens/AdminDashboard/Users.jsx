10ebaaa4b0aaf1517f73050122e119b9e495d9d0
Omri Naor <omrin2011@gmail.com>
2024-06-16 19:28:16 +0300

    Changed users and questionnaire management

--- src/screens/AdminDashboard/Users.jsx
+++ src/screens/AdminDashboard/Users.jsx
@@ -1,17 +1,14 @@
 // src/screens/AdminDashboard/Users.jsx
 import React, { useState, useEffect } from 'react';
 import './Users.css';
-import { FaEdit, FaTrashAlt, FaKey, FaPlus } from 'react-icons/fa';
+import { FaTrashAlt, FaKey } from 'react-icons/fa';
 import { db, auth } from '../../firebase/config'; 
 import { collection, getDocs, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { sendPasswordResetEmail } from 'firebase/auth';
-import { useSignup } from '../../hooks/useSignup';
-import { useNavigate } from 'react-router-dom';
 
 const Users = () => {
   const [users, setUsers] = useState([]);
-  const { signup, error } = useSignup();
-  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -23,10 +20,6 @@ const Users = () => {
 
     fetchUsers();
   }, []);
-
-  const handleEdit = (id) => {
-    alert(`ערוך משתמש ${id}`);
-  };
 
   const handleDelete = async (id, email) => {
     const confirmation = window.confirm(`האם אתה בטוח שברצונך למחוק את המשתמש ${email}?`);
@@ -63,30 +56,18 @@ const Users = () => {
     }
   };
 
-  const handleAddUser = async () => {
-    const email = prompt('הזן את האימייל של המשתמש החדש:');
-    const password = prompt('הזן את הסיסמה של המשתמש החדש:');
-    if (email && password) {
-      try {
-        await signup(email, password);
-        setUsers([...users, { id: email, email, isAdmin: false }]);
-        alert('המשתמש נוצר בהצלחה');
-      } catch (error) {
-        console.error('Error creating user: ', error);
-        alert(`שגיאה ביצירת המשתמש: ${error.message}`);
-      }
-    } else {
-      alert('נדרשים אימייל וסיסמה ליצירת משתמש חדש.');
-    }
-  };
+  const filteredUsers = users.filter(user => user.email.toLowerCase().startsWith(searchTerm.toLowerCase()));
 
   return (
     <div className='users'>
       <h1>משתמשים</h1>
-      <button className='add-user-button' onClick={handleAddUser}>
-        <FaPlus /> צור משתמש חדש
-      </button>
-      {error && <div className='error'>{error}</div>}
+      <input
+        type="text"
+        placeholder="חפש משתמש לפי אימייל"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className='search-input'
+      />
       <table className='users-table'>
         <thead>
           <tr>
@@ -96,7 +77,7 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map(user => (
+          {filteredUsers.map(user => (
             <tr key={user.id}>
               <td>{user.email}</td>
               <td>
@@ -107,9 +88,8 @@ const Users = () => {
                 />
               </td>
               <td>
-                <button onClick={() => handleEdit(user.id)}><FaEdit style={{ color: 'black' }} /></button>
-                <button onClick={() => handleDelete(user.id, user.email)}><FaTrashAlt style={{ color: 'black' }} /></button>
-                <button onClick={() => handleResetPassword(user.email)}><FaKey style={{ color: 'black' }} /></button>
+                <button title="מחק משתמש" onClick={() => handleDelete(user.id, user.email)}><FaTrashAlt style={{ color: 'black' }} /></button>
+                <button title="איפוס סיסמה" onClick={() => handleResetPassword(user.email)}><FaKey style={{ color: 'black' }} /></button>
               </td>
             </tr>
           ))}
