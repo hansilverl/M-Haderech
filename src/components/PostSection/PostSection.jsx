c8f456bb2283f7a4f9b24dd5358082beb906a1b2
Gil Yair Yamin <gil.yair.yamin@gmail.com>
2024-06-28 16:49:51 +0300

    merge into Post-Editor

--- src/components/PostSection/PostSection.jsx
+++ src/components/PostSection/PostSection.jsx
@@ -1,43 +1,65 @@
-import React from 'react'
+import React from 'react';
+import './PostSection.css';
+import Post from '../Posts/Post';
+import { useNavigate } from 'react-router-dom';
 
-import usePosts from '../../hooks/usePosts'
-import './PostSection.css'
-import Post from '../Post/Post'
-import { useNavigate } from 'react-router-dom'
 
 const PostsSection = () => {
-	const { posts, loading, error } = usePosts()
+  const navigate = useNavigate();
 
-	return (
-		<section id='posts' className='posts-section'>
-			<div className='posts-header'>
-				<h2>פוסטים</h2>
-				<div className='view-all-button-container'>
-					<button  className='view-all-button'>
-						ראה את כל הפוסטים
-					</button>
-				</div>
-			</div>
-			<div className='posts-container'>
-				{loading ? (
-					<h2>טוען...</h2>
-				) : ( error ? (
-					<h2>{error}</h2>
-				) : (
-					posts.slice(0, 3)
-						.map((post, index) => (
-							<Post
-								key={index}
-								image={post.image}
-								title={post.title}
-								date={post.date}
-								description={post.description}
-							/>
-						))
-				)) }
-			</div>
-		</section>
-	)
-}
+  const posts = [
+    {
+      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
+      title: 'פוסט 1',
+      date: '02/06/2024',
+      description: 'תיאור של פוסט 1'
+    },
+    {
+      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
+      title: 'פוסט 2',
+      date: '02/06/2024',
+      description: 'תיאור של פוסט 2'
+    },
+    {
+      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
+      title: 'פוסט 3',
+      date: '02/06/2024',
+      description: 'תיאור של פוסט 3'
+    },
+    {
+      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
+      title: 'פוסט 4',
+      date: '02/06/2024',
+      description: 'תיאור של פוסט 4'
+    }
+  ];
 
-export default PostsSection
+  const handleViewAllClick = () => {
+    navigate('/posts');
+  };
+
+
+  return (
+    <section id="posts" className="posts-section">
+      <div className="posts-header">
+        <h2>פוסטים</h2>
+        <div className="view-all-button-container">
+          <button onClick={handleViewAllClick} className="view-all-button">ראה את כל הפוסטים</button>
+        </div>
+      </div>
+      <div className="posts-container">
+        {posts.slice(0, 3).map((post, index) => (
+          <Post
+            key={index}
+            image={post.image}
+            title={post.title}
+            date={post.date}
+            description={post.description}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default PostsSection;
