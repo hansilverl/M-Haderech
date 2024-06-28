ab0a7038b32521809cab078eb8ec3c41925c4f49
Gil Yair Yamin <gil.yair.yamin@gmail.com>
2024-06-28 16:50:26 +0300

    cancelled edit mistake in PostSection

--- src/components/PostSection/PostSection.jsx
+++ src/components/PostSection/PostSection.jsx
@@ -1,65 +1,43 @@
-import React from 'react';
-import './PostSection.css';
-import Post from '../Posts/Post';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
 
+import usePosts from '../../hooks/usePosts'
+import './PostSection.css'
+import Post from '../Post/Post'
+import { useNavigate } from 'react-router-dom'
 
 const PostsSection = () => {
-  const navigate = useNavigate();
+	const { posts, loading, error } = usePosts()
 
-  const posts = [
-    {
-      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
-      title: 'פוסט 1',
-      date: '02/06/2024',
-      description: 'תיאור של פוסט 1'
-    },
-    {
-      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
-      title: 'פוסט 2',
-      date: '02/06/2024',
-      description: 'תיאור של פוסט 2'
-    },
-    {
-      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
-      title: 'פוסט 3',
-      date: '02/06/2024',
-      description: 'תיאור של פוסט 3'
-    },
-    {
-      image: 'https://assets1.farmaciasanpablo.com.mx/landings/_blog/bebes/230125-10cuidadosEmbarazada/10-cuidados-que-debe-tener-una-mujer-embarazada.jpg',
-      title: 'פוסט 4',
-      date: '02/06/2024',
-      description: 'תיאור של פוסט 4'
-    }
-  ];
+	return (
+		<section id='posts' className='posts-section'>
+			<div className='posts-header'>
+				<h2>פוסטים</h2>
+				<div className='view-all-button-container'>
+					<button  className='view-all-button'>
+						ראה את כל הפוסטים
+					</button>
+				</div>
+			</div>
+			<div className='posts-container'>
+				{loading ? (
+					<h2>טוען...</h2>
+				) : ( error ? (
+					<h2>{error}</h2>
+				) : (
+					posts.slice(0, 3)
+						.map((post, index) => (
+							<Post
+								key={index}
+								image={post.image}
+								title={post.title}
+								date={post.date}
+								description={post.description}
+							/>
+						))
+				)) }
+			</div>
+		</section>
+	)
+}
 
-  const handleViewAllClick = () => {
-    navigate('/posts');
-  };
-
-
-  return (
-    <section id="posts" className="posts-section">
-      <div className="posts-header">
-        <h2>פוסטים</h2>
-        <div className="view-all-button-container">
-          <button onClick={handleViewAllClick} className="view-all-button">ראה את כל הפוסטים</button>
-        </div>
-      </div>
-      <div className="posts-container">
-        {posts.slice(0, 3).map((post, index) => (
-          <Post
-            key={index}
-            image={post.image}
-            title={post.title}
-            date={post.date}
-            description={post.description}
-          />
-        ))}
-      </div>
-    </section>
-  );
-};
-
-export default PostsSection;
+export default PostsSection
