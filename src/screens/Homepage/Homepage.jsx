526d0f9f0c101ca526ea1661e5b456bf3ff5e7c7
Administrator <admin@example.com>
2024-06-29 23:22:53 +0300

    chore: Update donation section image and align it to the left

--- src/screens/Homepage/Homepage.jsx
+++ src/screens/Homepage/Homepage.jsx
@@ -42,7 +42,7 @@ const HomePage = () => {
     <div className="homepage" dir="rtl">
       <header className="header">
         <div className="header-image-container">
-          <img src="https://images.inc.com/uploaded_files/image/1920x1080/evan-kirby-101570_199211.jpg" alt="Em Haderech" className="header-image" />
+          <img src="https://images.unsplash.com/photo-1505679208891-9ab12ee61dc1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Em Haderech" className="header-image" />
           <div className="header-text">
             <h1>ברוכים הבאים לאם הדרך</h1>
             <button onClick={handleFormClick} className="fill-form-button">מילוי טופס</button>
