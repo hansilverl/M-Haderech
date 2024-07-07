7e78586411f5b7eed69c38067c5399def9b16dc7
Omri Naor <omrin2011@gmail.com>
2024-07-07 12:17:02 +0300

    Changed statistics to enable multiple + change some components to responsive + added domain suggestions to login

--- src/components/AnalyticsSection/AnalyticsSection.jsx
+++ src/components/AnalyticsSection/AnalyticsSection.jsx
@@ -3,11 +3,16 @@ import CountUp from 'react-countup';
 import useStatistics from '../../hooks/useStatistics';
 import './AnalyticsSection.css';
 
-const AnalyticsSection = () => {
+const AnalyticsSection = ({ animate }) => {
   const { statistics, loading, error } = useStatistics();
 
   if (loading) return <p>טוען...</p>;
   if (error) return <p>שגיאה: {error}</p>;
+
+  // Sort statistics by value
+  const sortedStatistics = statistics
+    ? Object.entries(statistics).sort((a, b) => b[1] - a[1])
+    : [];
 
   return (
     <section id="analytics" className="analytics-section">
@@ -15,15 +20,23 @@ const AnalyticsSection = () => {
         <h2>נתונים של העמותה</h2>
         <p>כאן ניתן לראות את נתונים שלנו לאורך החודשים האחרונים...</p>
       </div>
-      <div className="analytics-number">
-        <h3>מספר אמהות שעזרנו להן</h3>
-        <CountUp
-          start={0}
-          end={statistics.helped_mothers_amount || 0}
-          duration={3}
-          separator=","
-          className="countup-number"
-        />
+      <div className="analytics-numbers">
+        {sortedStatistics.map(([key, value]) => (
+          key !== 'id' && (
+            <div key={key} className="analytics-number">
+              <h3>{key}</h3>
+              {animate && (
+                <CountUp
+                  start={0}
+                  end={Number(value) || 0}
+                  duration={3}
+                  separator=","
+                  className="countup-number"
+                />
+              )}
+            </div>
+          )
+        ))}
       </div>
     </section>
   );
