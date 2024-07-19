40eb655d6cb31e0dc9ad70cb90c2fae26b25a9c5
Administrator <admin@example.com>
2024-07-19 10:34:47 +0300

    localize header image

--- src/screens/Homepage/Homepage.jsx
+++ src/screens/Homepage/Homepage.jsx
@@ -8,6 +8,7 @@ import NewsletterLink from '../../components/NewsletterLink/NewsletterLink'; // 
 import { useNavigate } from 'react-router-dom';
 import { db } from '../../firebase/config';
 import { doc, getDoc } from 'firebase/firestore';
+import headerImage from '../../assets/michal-bar-haim-NYvRaxVZ-_M-unsplash.jpg';
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -67,7 +68,8 @@ const HomePage = () => {
     <div className="homepage" dir="rtl">
       <header className="header">
         <div className="header-image-container">
-          <img src="https://images.unsplash.com/photo-1505679208891-9ab12ee61dc1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Em Haderech" className="header-image" />
+          <img src={headerImage} alt="Em Haderech" className="header-image" />
+          {/* <img src="https://images.unsplash.com/photo-1505679208891-9ab12ee61dc1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Em Haderech" className="header-image" /> */}
           <div className="header-text">
             <h1>ברוכות הבאות לאם הדרך </h1>
             <h3>
