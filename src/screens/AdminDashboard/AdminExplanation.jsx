13f6a5c399f3d777ecece9e943a4a4ba87f3e0c1
Administrator <admin@example.com>
2024-07-20 22:54:29 +0300

    Change ordder

--- src/screens/AdminDashboard/AdminExplanation.jsx
+++ src/screens/AdminDashboard/AdminExplanation.jsx
@@ -10,8 +10,8 @@ const AdminExplanation = () => {
         <li className="category-item"><strong>ניהול תוכן:</strong> ניהול המאמרים והאירועים של האתר, כולל יצירה, עריכה ומחיקה של מאמרים ואירועים.</li>
         <li className="category-item"><strong>משתמשים:</strong> ניהול המשתמשים הרשומים באתר, כולל הגדרת הרשאות, ואיפוס סיסמאות.</li>
         <li className="category-item"><strong>ניהול מבדקים:</strong> ניהול המבדקים, כולל הוספה, עריכה ומחיקה של שאלות ותשובות.</li>
-        <li className="category-item"><strong>שונות:</strong> מידע כללי וסטטיסטיקות שונות כגון פרטי בנק, אודות יצירת קשר ועוד.</li>
         <li className="category-item"><strong>היסטוריית מבדקים:</strong> צפייה בהיסטוריית מבדקים ומעקב אחר תשובות המשתמשים.</li>
+        <li className="category-item"><strong>שונות:</strong> מידע כללי וסטטיסטיקות שונות כגון פרטי בנק, אודות יצירת קשר ועוד.</li>
       </ul>
     </div>
   );
