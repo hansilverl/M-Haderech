ab240bc4c919f7829b54d00494aebf825155ab60
Administrator <admin@example.com>
2024-10-07 15:20:42 +0300

    Refactor PDF icon and title in AdminExplanation component

--- src/screens/AdminDashboard/AdminExplanation.jsx
+++ src/screens/AdminDashboard/AdminExplanation.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './AdminExplanation.css';
+import pdfIcon from '../../assets/pdf-file.png'; // Importing the PDF icon
 
 const AdminExplanation = () => {
   return (
     <div className="admin-explanation" dir="rtl">
+      {/* PDF Icon with title under it */}
+      <div className="pdf-container-explain">
+        <a href={require('../../assets/Site guide.pdf')} target="_blank" rel="noopener noreferrer">
+          <img src={pdfIcon} alt="PDF Icon" className="pdf-icon-explain" />
+        </a>
+        <span className="pdf-title-explain">
+          <a href={require('../../assets/Site guide.pdf')} target="_blank" rel="noopener noreferrer" className="pdf-link-explain">
+          מעבר למדריך המפורט
+          </a>
+          </span>
+      </div>
+      
       <h1 className="welcome-header">ברוכות הבאות ללוח הניהול</h1>
       <p className="intro-text">כאן תוכלו לנהל את האתר ולבצע פעולות שונות בהתאם לקטגוריות המפורטות:</p>
       <ul className="category-list">
